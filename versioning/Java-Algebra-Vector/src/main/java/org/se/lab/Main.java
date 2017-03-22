36f5086316bd42c1bb136d0300cb57ff3d5f5bfc
teiniker <egon.teiniker@fh-joanneum.at>
2017-03-22 15:06:24 +0100

    Changed test data to 7

--- versioning/Java-Algebra-Vector/src/main/java/org/se/lab/Main.java
+++ versioning/Java-Algebra-Vector/src/main/java/org/se/lab/Main.java
@@ -4,8 +4,8 @@ public class Main
 {
     public static void main(String... args)
     {
-        Vector v1 = new Vector(1,2,3,4,5);
-        Vector v2 = new Vector(3,2,1,4,5);
+        Vector v1 = new Vector(1,2,3,4,7);
+        Vector v2 = new Vector(3,2,1,4,7);
         
         System.out.println("v1 = " + v1);
         System.out.println("v2 = " + v2);
